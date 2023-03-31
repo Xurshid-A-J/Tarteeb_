66263d2b2aa1f4fb97d2d086d66c0b803d48578d
Xurshid-A-J <abdxurshid02@gmail.com>
2023-03-31 22:09:13 +0500

    ShouldThrowValidationExceptionOnModifyIfScoreIsNullAndLogItAsync -> FAIL

---
+++ Tarteeb.Api/Models/Foundations/Scores/Exceptions/NullScoreException.cs
@@ -0,0 +1,18 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free to use to bring order in your workplace
+//=================================
+
+using System;
+using Xeptions;
+
+namespace Tarteeb.Api.Models.Foundations.Scores.Exceptions
+{
+    public class NullScoreException : Xeption
+    {
+        public NullScoreException()
+            : base(message: "Score is null.")
+        { }
+    }
+}
+
